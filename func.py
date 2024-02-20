3497f0e0c666034079f3a99488ad899ca047d0ac
SagarSinghThapa <157865147+SagarSinghThapa@users.noreply.github.com>
2024-02-20 11:42:21 +0530

    Create func.py

---
+++ func.py
@@ -0,0 +1,9 @@
+def func_1(b,c):
+  add=b+c
+  subtract=b-c
+  multiply=b*c
+  divide=b/c
+  return add,subtract,multiply,divide
+b=int(input("Enter the first value"))
+c=int(input("Enter the second value"))
+func_1(b,c)
